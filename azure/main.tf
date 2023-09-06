89a4b8540235c21e540c1aeca81e7d1bbc11359e
Paulo Alexandre Dias Farias <pauloalexandre820@gmail.com>
2023-09-06 17:46:40 -0300

    Add files via upload

--- azure/main.tf
+++ azure/main.tf
@@ -2,9 +2,11 @@ terraform {
 
   required_version = ">= 1.0.0"
 
-  required_providers {
-    source  = "hashicorp/azurerm"
-    version = "=3.0.0"
+   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
   }
 }
 
--- azure/storage-account.tf
+++ azure/storage-account.tf
@@ -2,20 +2,20 @@ resource "azurerm_resource_group" "first_resource_group" {
   name     = ""
   location = var.location
 
-  tags = ""
+  tags = local.common_tags
 }
 
 resource "azurerm_storage_account" "first_storage_account" {
-  name                     = ""
-  resource_group_name      = ""
+  name                     = "pauloalexandrestorageaccount"
+  resource_group_name      = azurerm_resource_group.first_resource_group
   location                 = var.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
 
-  tags = ""
+  tags = local.common_tags
 }
 
 data "azurerm_storage_container" "first_container" {
   name                 = ""
-  storage_account_name = ""
+  storage_account_name = azurerm_storage_account.first_storage_account.name
 }
