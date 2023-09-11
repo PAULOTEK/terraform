d31aa3f919cae251507515e1456d62f979a26a06
Paulo.farias <paulo.farias@compart.com.br>
2023-09-11 16:10:13 -0300

    alteracoes aws vm

---
+++ aws_vm/main.tf
@@ -0,0 +1,37 @@
+terraform {
+
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-bucket"
+    key    = "aws-vm,/terraform.tfstate"
+    region = "eu-central-1"
+  }
+
+}
+
+provider "aws" {
+  region = "eu-central-1"
+  default_tags {
+    tags = {
+      owner      = "pauloalexandre"
+      managed-by = "terraform"
+    }
+  }
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "terraform-bucket"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
---
+++ aws_vm/vm.tf
@@ -0,0 +1,16 @@
+resource "aws_key_pair" "key" {
+  public_key = file("")
+  key_name   = "aws_kay"
+}
+resource "aws_instance" "vm" {
+  ami           = "ami-"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.key.key_name
+  subnet_id     = data.terraform_remote_state.vpc.outputs.subnet.id
+  vpc_security_group_ids = [data.terraform_remote_state.vpc.outputs.security_group_id]
+  associate_public_ip_address = true
+
+  tags = {
+    "Name" = "vm_terraform"
+  }
+}
